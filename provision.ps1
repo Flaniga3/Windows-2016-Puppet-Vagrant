bb8066ce0e2cdc6eff1c7e1eddedea531a7d3f89
Joseph Flanigan <joeflanigan@northwesternmutual.com>
2018-05-29 13:00:11 -0500

    Initial commit of functioning code

---
+++ provision.ps1
@@ -0,0 +1,5 @@
+Invoke-WebRequest -Uri 'http://downloads.puppetlabs.com/windows/puppet5/puppet-agent-x64-latest.msi' -Outfile C:\Windows\Temp\puppet-installer.msi
+Start-Process C:\Windows\Temp\puppet-installer.msi -Wait -ArgumentList "/qn PUPPET_MASTER_SERVER=puppet PUPPET_AGENT_CERTNAME=windows-test-agent-$((Get-Date -Format FileDateTime).ToLower())"
+Stop-Service -Name puppet
+Add-Content C:\Windows\System32\drivers\etc\hosts "10.0.2.2 puppet"
+puppet config set --section agent masterport=32771
